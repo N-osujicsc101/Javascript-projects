9afe82501c64509eea335c7ad307a24b0620d214
Nneoma Osuji <115807216+N-osujicsc101@users.noreply.github.com>
2023-10-31 22:41:34 +0100

    currency converter

---
+++ currency.js
@@ -0,0 +1,14 @@
+// Written with 💖
+let yuan = 7.19
+let yen = 141.45
+let won = 1279.11
+
+yuan = 500/7.19
+yen  = 500/141.45
+won  = 500/1279.11
+
+totalUsd = yuan + yen + won
+console.log('What do you have left in yuan?', yuan)
+console.log('What do you have left in yen?', yen)
+console.log('What do you have left in won?', won)
+console.log(totalUsd)
